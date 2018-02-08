0281c8cf5d7aeeb93a864b18c2d7c664482252f6
Shubham Yadav <shubhamyadav03.2011@gmail.com>
2018-02-09 01:17:34 +0530

    Second commit

--- Desktop/internshala/app.js
+++ Desktop/internshala/app.js
@@ -1,43 +1,32 @@
+var app = angular.module('MyApp' , ['ui.router']);
 
-var app = angular.module('MyApp', ["ui-router"]);
+app.config(function($stateProvider , $urlRouterProvider, $locationProvider){
+    $urlRouterProvider.otherwise("/");
+    Stamplay.init("shubham1997");
+    $locationProvider.hashPrefix("");
 
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'template/home.html',
+            controller: "HomeCtrl"
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'template/login.html',
+            controller: "LoginCtrl"
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'template/signup.html',
+            controller: "SignUpCtrl"
+        });
 
-app.config(function($stateProvider , $urlRouterProvider){
-	
-	Stamplay.init("shubham1997");
-	
-	$locationProvider.hashPrefix('');
-	$urlRouterProvider.otherwise("/");
-	$stateProvider
-	.state('home',{
-		url : '/',
-		templateUrl : 'template/home.html',
-		controller : "HomeCtrl"
-	})
-
-	.state('login',{
-		url:'/login',
-		templateUrl : 'template/login.html' ,
-		controller : "LoginCtrl" 
-	})
-	.state('signup',{
-		url : '/signup',
-		templateUrl : 'template/signup.html',
-		controller : "SignUpCtrl"
-	})
-	
+    
 });
 
-app.controller('MyCtrl', function($scope) {	
-
-})
-
-app.controller('MyBlogsCtrl', function($scope) {	
-
-})
-
-app.controller('HomeCtrl', function($scope) {
-	
+app.controller('HomeCtrl',function($scope){
+    
 });
 
 app.controller('LoginCtrl', function($scope) {
@@ -66,27 +55,36 @@ app.controller('LoginCtrl', function($scope) {
 	}
 });
 
-app.controller('SignUpCtrl', function($scope) {
-		$scope.newUser = {};
-		$scope.signup = function(){
-			if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmpassword){
-				console.log("All fields are valid!");
+app.controller('SignUpCtrl',function($scope){
+    $scope.newUser = {};
 
-				if($scope.newUser.password == $scope.newUser.confirmpassword){
-					console.log("All good! Let's sign up!");
-					Stamplay.User.signup($scope.newUser)
-					.then(function(response){
-						console.log(response);
-					}, function(error){
-						console.log(error);
-					});
-				}
-				else{
-					console.log("Pasword do not match");
-				}
-			}
-			else{
-				console.log("Some fields are invalid!");
-			}
-		}
+    $scope.signup = function(){
+        if($scope.newUser.firstName && $scope.newUser.lastName && $scope.newUser.email && $scope.newUser.password && $scope.newUser.confirmpassword){
+            console.log("All fields are valid!");
+
+            if($scope.newUser.password == $scope.newUser.confirmpassword){
+                console.log("All good! Let's sign up!");
+                Stamplay.User.signup($scope.newUser)
+                .then(function(response){
+                    console.log(response);
+                }, function(error){
+                    console.log(error);
+                });
+            }
+            else{
+                console.log("Pasword do not match");
+            }
+        }
+        else{
+            console.log("Some fields are invalid!");
+        }
+    }
+});
+
+app.controller('MyCtrl',function($scope){
+    
+});
+
+app.controller('MyBlogCtrl',function($scope){
+    
 });
